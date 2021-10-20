171cfe0d8d94d73dc49212e104cafd8d95989bae
Tom Fondville <tom.fondville@etu.umontpellier.fr>
2021-10-20 16:38:58 +0200

    .

--- src/main/java/fr/umontpellier/iut/gymkhana/Partie.java
+++ src/main/java/fr/umontpellier/iut/gymkhana/Partie.java
@@ -53,33 +53,9 @@ public class Partie {
     }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
     public static void main(String[] args) {
 
         Partie p = new Partie();
         p.intit();
-
-//       Plateau plateau = new Plateau(5);
-//
-//       plateau.getGrapheBlanc().getPointCord(3,3).addVoisin(plateau.getGrapheBlanc().getPointCord(2,3));
-//        plateau.getGrapheRouge().getPointCord(3,3).addVoisin(plateau.getGrapheRouge().getPointCord(2,3));
-//        System.out.println(plateau);
-
-
-//        String str = "\u001B[31m" + "tedgz" + "\u001B[0m" + "dede";
-//        System.out.println(str);
-
     }
 }

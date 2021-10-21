54c5ecf37a54f8ba339a535b27ebca4df9862bd0
Vasileiadis Kimon <kimon.vasileiadis@etu.umontpellier.fr>
2021-10-21 16:40:39 +0200

    Second

--- src/main/java/fr/umontpellier/iut/gymkhana/Test.java
+++ src/main/java/fr/umontpellier/iut/gymkhana/Test.java
@@ -1,2 +1,20 @@
-package fr.umontpellier.iut.gymkhana;public class Test {
+package fr.umontpellier.iut.gymkhana;
+
+import de.gurkenlabs.litiengine.Game;
+import de.gurkenlabs.litiengine.environment.Environment;
+
+import java.awt.geom.Rectangle2D;
+
+public class Test {
+
+    public static void main(String[] args) {
+        Environment env = new Environment()
+        Plateau p = new Plateau(5);
+        p.getGrapheBlanc().getPointCord(3,3).addVoisin(p.getGrapheBlanc().getPointCord(2,3));
+        p.getGrapheRouge().getPointCord(3,3).addVoisin(p.getGrapheRouge().getPointCord(2,3));
+        Game.graphics().renderShape(p, );
+        Game.init(args);
+        Game.start();
+    }
 }
+

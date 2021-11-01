4817e06526b370917775c85600b5fab56b8a1bd9
louis-marceron <72874947+louis-marceron@users.noreply.github.com>
2021-11-01 20:26:17 +0100

    Documentation Plateau.java + constructeur de plateau

--- src/main/java/fr/umontpellier/iut/gymkhana/Plateau.java
+++ src/main/java/fr/umontpellier/iut/gymkhana/Plateau.java
@@ -3,38 +3,52 @@ package fr.umontpellier.iut.gymkhana;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+/**
+ * La classe {@code Plateau} permet de créer instances d'un plateau du Gymkhana, modélisé
+ * à travers une matrice d'incidence de type {@code String[][]}.
+ * Cette matrice comprend la position de chaque sommet et de chaque arrête placée, représentés par
+ * une chaine de caractères.
+ * <p><b>Légende</b>
+ * <blockquote>
+ * NA = case non assignée (correspond aux 4 coins du plateau)<br>
+ * V = case vide<br>
+ * SR = sommet rouge<br>
+ * SB = sommet blanc<br>
+ * AR = arrête rouge<br>
+ * AB = arrête blanche<br>
+ * </blockquote>
+ * <p>La classe {@code Plateau} comprend les méthodes pouvant placer des arrêtes et vérifier si un
+ * {@code Joueur} est gagnant. Il y aussi des méthodes spécifiques aux Graphes, pouvant par exemple
+ * retourner les voisins d'un sommet ou sa classe de connexité.
+ */
 public class Plateau {
+    private final int taille;
     private Joueur joueurRouge;
     private Joueur joueurBlanc;
-    private final int taille;
     private String[][] matrice;
 
-    /*
-    L'attribut "matrice" correpond à une matrice comprenant les sommets rouges et blancs, et leurs arrêtes respectives
-    NA = case non assignée (correspond aux 4 coins du plateau)
-    V = case vide
-    SR = sommet rouge
-    SB = sommet blanc
-    AR = arrête rouge
-    AB = arrête blanche
-    */
-
+    /**
+     * Initialise un nouvel objet {@code Plateau} en construisant une matrice comprenant les cases
+     * non assignées "NA", les cases vides "V" et les sommets rouges et blancs "SR" / "SB".
+     *
+     * @param taille
+     *        La longueur des graphes Rouge et Blanc. Leur largeur est de {@code taille - 1}
+     */
     public Plateau(int taille) {
         this.taille = taille;
 
         int dimension = taille * 2 + 1;
         matrice = new String[dimension][dimension];
 
-        // On remplit le tableau de "V"
+        // On remplit la matrice de "V"
         for (String[] strings : matrice) Arrays.fill(strings, "V");
 
-        // On met "NA" dans les quatres coins de la matrice
         matrice[0][0] = "NA";
         matrice[matrice.length - 1][0] = "NA";
         matrice[0][matrice.length - 1] = "NA";
         matrice[matrice.length - 1][matrice.length - 1] = "NA";
 
-        // On place tous les sommets
+        // On place les sommets rouges et blancs
         for (int i = 0; i < matrice.length; i++) {
             for (int j = 0; j < matrice[i].length; j++) {
                 if (i % 2 == 0) {
@@ -49,27 +63,18 @@ public class Plateau {
         }
     }
 
-    public int getTaille() {
-        return taille;
-    }
-
-    public String[][] getMatrice() {
-        return matrice;
-    }
-
-    @Override
-    public String toString() {
-        return ImpressionPlateau.impression(this);
-    }
-
     public boolean ajouterArrete(int[] a1, int[] a2, Couleur c) {
         // TODO try/catch + tests unitaires
         // les indices ne sont pas trop grands
-        if (a1[0] > taille * 2 || a1[1] > taille * 2 || a2[0] > taille * 2 || a2[1] > taille * 2)
+        if (a1[0] > taille * 2
+                || a1[1] > taille * 2
+                || a2[0] > taille * 2
+                || a2[1] > taille * 2)
             return false;
 
         // les deux cases sont toutes les deux SR ou toutes les deux SB
-        if (!matrice[a1[0]][a1[1]].equals(c.nomSommet()) || !matrice[a2[0]][a2[1]].equals(c.nomSommet()))
+        if (!matrice[a1[0]][a1[1]].equals(c.nomSommet())
+                || !matrice[a2[0]][a2[1]].equals(c.nomSommet()))
             return false;
 
         // il y a une case d'écart entre les deux cases
@@ -84,7 +89,7 @@ public class Plateau {
         return true;
     }
 
-    public ArrayList<int[]> voisinsDunSommet(int[] s, Couleur c) {
+    public ArrayList<int[]> getVoisinsSommet(int[] s, Couleur c) {
         // TODO tests unitaires
         ArrayList<int[]> voisins = new ArrayList<>();
 
@@ -106,5 +111,18 @@ public class Plateau {
             voisins.add(new int[]{s[0] + 2, s[1]});
 
         return voisins;
+    }
+
+    public int getTaille() {
+        return taille;
+    }
+
+    public String[][] getMatrice() {
+        return matrice;
+    }
+
+    @Override
+    public String toString() {
+        return ImpressionPlateau.impression(this);
     }
 }
